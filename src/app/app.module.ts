5e6d697cd38ae6576e5395723d630501be24be7a
mananqayas <mrmuhammadqayas@gmail.com>
2024-08-22 00:57:57 -0400

    module version

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { UserInputModule } from './user-input/user-input.module';
+@NgModule({
+  declarations: [AppComponent, InvestmentResultsComponent, HeaderComponent],
+  imports: [FormsModule, BrowserModule, UserInputModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
